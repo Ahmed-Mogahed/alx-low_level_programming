f205c87b0146d585728004b8446a4fa05d9283c7
Ahmed Ayman <aayman3224@gmail.com>
2023-07-11 16:58:31 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,8 +12,8 @@ int count_word(char *s)
 {
 	int flag, c, w;
 
-	flag = 0;
-	w = 0;
+		flag = 0;
+		w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
@@ -21,7 +21,7 @@ int count_word(char *s)
 			flag = 0;
 		else if (flag == 0)
 		{
-flag = 1;
+			flag = 1;
 			w++;
 		}
 	}
