90bd34d0fa2771191bb77612d8a98e12445b9662
Ahmed Ayman <aayman3224@gmail.com>
2023-07-11 16:33:44 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,35 +13,40 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int n, i, len1, len2, len0;
-	char *result;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len1 = len2 = 0;
-
-	if (s1 != NULL)
-{
-	n = 0;
-	while (s1[n++] != '\0')
-	len1++;
-}
-
-		if (s2 != NULL)
-	{
-	n = 0;
-	while (s2[n++] != '\0')
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
 		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
 
-	len0 = len1 + len2;
-	result = (char *)malloc(sizeof(char) * (len0 + 1));
-	if (result == NULL)
-	return (NULL);
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
 
-	for (n = 0; n < len1; n++)
-		result[n] = s1[n];
-	for (i = 0; i < len2; i++, n++)
-		result[n] = s2[i];
-		result[len0] = '\0';
-
-	return (result);
+	return (s3);
 }
