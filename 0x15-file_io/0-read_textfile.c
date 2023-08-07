48cf90de394ec5ddd73179cab0d81271bd8fc959
Ahmed Ayman <aayman3224@gmail.com>
2023-08-07 12:09:09 -0700

    C - File I/O](0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
- * Return: n- actual number of bytes read and printed
+ * Return: w- actual number of bytes read and printed
  *         0 when function fails or filename is NULL.
  */
 
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t n;
 	ssize_t u;
 
-	f = open(filenamen, O_RDONLY);
+	f = open(filename, O_RDONLY);
 	if (f == -1)
 		return (0);
 	b = malloc(sizeof(char) * letters);
