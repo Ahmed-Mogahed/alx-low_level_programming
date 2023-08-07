fa979f42cd374f2045e14543ba11a4f9930129a5
Ahmed Ayman <aayman3224@gmail.com>
2023-08-07 11:54:43 -0700

    C - File I/O](0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,11 +7,11 @@ void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @f: The name of the file buffer is storing chars for.
+ * @file: The name of the file buffer is storing chars for.
  * Return: A pointer to the newly-allocated buffer.
  */
 
-char *create_buffer(char *f)
+char *create_buffer(char *file)
 {
 	char *buffer;
 
@@ -20,7 +20,7 @@ char *create_buffer(char *f)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", f);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -50,12 +50,12 @@ void close_file(int fd)
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  * Return: 0 on success.
- *
  * Description: If the argument count is incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
  */
+
 int main(int argc, char *argv[])
 {
 	int fr, t, k, u;
