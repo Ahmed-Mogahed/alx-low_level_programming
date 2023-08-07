b8b02b1ac56b10d015be604c9b4fceeec7c3dad1
Ahmed Ayman <aayman3224@gmail.com>
2023-08-07 11:59:15 -0700

    C - File I/O](0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,7 +39,7 @@ void close_file(int fd)
 	b = close(fd);
 
 	if (b == -1)
-	{
+{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
@@ -49,16 +49,17 @@ void close_file(int fd)
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
+ *
  * Return: 0 on success.
+ *
  * Description: If the argument count is incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
  */
-
 int main(int argc, char *argv[])
 {
-	int fr, t, k, u;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -68,12 +69,12 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	fr = open(argv[1], O_RDONLY);
-	k = read(fr, buffer, 1024);
-	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (fr == -1 || k == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -81,8 +82,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		u = write(t, buffer, k);
-		if (t == -1 || u == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -90,14 +91,14 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		k = read(fr, buffer, 1024);
-		t = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (k > 0);
+	} while (r > 0);
 
-	fr(buffer);
-	close_file(fr);
-	close_file(t);
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
